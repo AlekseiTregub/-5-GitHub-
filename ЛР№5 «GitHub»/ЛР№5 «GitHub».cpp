699623a8c093aa8606c6fbd05e069a1c0329c03d
AlekseiTregub <127427827+AlekseiTregub@users.noreply.github.com>
2023-05-16 11:46:14 +0300

    Update ЛР№5 «GitHub».cpp
    Створено 5 Hello Word

--- ЛР№5 «GitHub»/ЛР№5 «GitHub».cpp
+++ ЛР№5 «GitHub»/ЛР№5 «GitHub».cpp
@@ -5,4 +5,6 @@ int main()
     std::cout << "1 Hello World!\n";
     std::cout << "2 Hello World!\n";
     std::cout << "3 Hello World!\n";
+    std::cout << "4 Hello World!\n";
+    std::cout << "5 Hello World!\n";
 }
